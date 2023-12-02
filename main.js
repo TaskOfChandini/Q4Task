fa17f181fbc96b2a2b575ce46c4161b4d4a4a3b4
TaskOfChandini <152705057+TaskOfChandini@users.noreply.github.com>
2023-12-02 19:27:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// Simulated user data stored in JSON format
+const users = [
+  { username: 'user1', password: 'password1' },
+  { username: 'user2', password: 'password2' },
+  // Add more users as needed
+];
+
+function signup(username, password) {
+  // Check if the username is already taken
+  const userExists = users.some(user => user.username === username);
+
+  if (!userExists) {
+    // Add the new user to the list
+    users.push({ username, password });
+    console.log('Signup successful!');
+  } else {
+    console.error('Username already taken. Please choose another.');
+  }
+}
